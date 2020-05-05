d315f2534ce86476b55e7e573bda49abd07066e6
daanaubel <daanaubel@gmail.com>
2020-05-05 10:26:52 +0200

    date

--- todos/models.py
+++ todos/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class Todo(models.Model):
     title = models.CharField(max_length=100)
     completed = models.BooleanField(default=False)
+    dueDate = models.DateTimeField(blank=True, null=True)
     owner = models.ForeignKey(
         User, related_name='todos', on_delete=models.CASCADE, null=True)
 
